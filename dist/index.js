fb85f46c5cb23e0063809a71f0e6e5234ca1f5a8
mahmoudramadan0040 <mahmoudramadan0040@gmail.com>
2022-02-11 02:31:18 +0200

    add jasmine

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("hello world");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const PORT = 3000;
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+});
+exports.default = app;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-console.log('hello world');
+import express from 'express'
 
 
 
+const app = express()
+
+const PORT = 3000
+
+app.get("/",(req:express.Request,res:express.Response)=>{
+    res.send("hello world");
+});
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+});
+
+export default app;
